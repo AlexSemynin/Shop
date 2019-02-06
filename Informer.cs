f065403e4c699e469c55df7e39c39a878c6af131
AlexSemynin <alex.semynin97@mail.ru>
2019-02-07 02:02:10 +0300

    Добавлен Класс для реализации покупки

---
+++ Informer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_2_Polimorfizm
+{
+    class Informer
+    {
+        public void Buy(User user, Product product)
+        {
+            double price = product.GetDiscountPrice(user);
+            user.ReduceBalance(price);
+            Console.WriteLine($"{user.Name} buy {product.Name} за {price}.");
+        }   
+    }
+}
