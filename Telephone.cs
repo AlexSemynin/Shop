63c45106369d6235ed8dc3f89a8ab5d366ac10ca
AlexSemynin <alex.semynin97@mail.ru>
2019-02-06 23:01:05 +0300

    Добавлен дочерний от продуктов класс Телефон, где добавлено свойство наличия кнопок, а так же конструктор наследуемый от родительского при помощи base

---
+++ Telephone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_2_Polimorfizm
+{
+    class Telephone : Product
+    {
+        public bool HaveButtons { get; set; }
+
+        //Конструктор от родителя
+        public Telephone(string name, double price, string manufacture, bool haveButtons) 
+            : base(name, price, manufacture)
+        {
+            HaveButtons = haveButtons;
+        }
+    }
+}
