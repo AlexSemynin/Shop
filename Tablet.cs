903176d71bcab269352a51ad2135fbfae0db33fb
AlexSemynin <alex.semynin97@mail.ru>
2019-02-06 23:07:23 +0300

    Добавлен ксласс Планшеты

---
+++ Tablet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_2_Polimorfizm
+{
+    class Tablet : Product
+    {
+        public string Size { get; set; }
+
+        public Tablet(string name, double price, string manufacture, string size)
+            : base(name, price, manufacture)
+        {
+            Size = size;
+        }
+    }
+}
