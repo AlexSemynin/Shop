1d93954866da2f1eed384c56913bf512165ba909
AlexSemynin <alex.semynin97@mail.ru>
2019-02-06 22:31:30 +0300

    Создан магазин и объект пользователь

---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_2_Polimorfizm
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public double Balance { get; set; }
+        public double Spent { get; set; }
+        
+        //Конструктор
+        public User(string name, string adr, double balance, double spent)
+        {
+            Name = name;
+            Adress = adr;
+            Balance = balance;
+            Spent = spent;
+        }
+
+        //метод
+        public void ReduceBalance(double price)
+        {
+            Balance -= price;
+            Spent += price;
+        }
+    }
+}
