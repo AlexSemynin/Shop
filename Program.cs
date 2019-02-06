d6c646ebc1768d8a0ee06e120e1c4278296c1701
AlexSemynin <alex.semynin97@mail.ru>
2019-02-07 01:17:14 +0300

    Сделал массив продуктов Upcast-ом, и добавил функцию вывода этого Ассортимента на экран

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace _4_4_2_Polimorfizm
     {
         static void Main(string[] args)
         {
+            User user1 = new User("Вася", "50letOktyabrya", 10000, 0); //Эта информация берется из БД
+
+            Telephone iphone = new Telephone("IPhone XV", 7000, "Apple", false);
+            Telephone samsung = new Telephone("Galaxy XIV", 11000, "Samsung", false);      //Эта тоже, наверн)
+            Tablet tablet1 = new Tablet("OneXRSERT", 10000, "Samsung", "10x10x2");
+            Laptop laptop1 = new Laptop("Mac", 2000000000, "Apple", "Steel");
+
+            //upcast т.к. тип массива Product, а я пихаю туда значения других типов, !но! дочерних 
+            Product[] products = new Product[]
+            {
+                iphone,
+                samsung,
+                tablet1,
+                laptop1
+            };
+            
+        }
+        public void Assortiment(Product[] products)
+        {
+            Console.WriteLine("Список товаров:");
+            for(int i = 0; i < products.Length; i++)
+            {
+                products[i].DispInfoProduct();
+                Console.WriteLine("_________________");
+            } 
         }
     }
 }
