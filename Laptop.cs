8c2f2da352b4058b2ec26b0d2e8d1552f3f0f0f3
AlexSemynin <alex.semynin97@mail.ru>
2019-02-06 23:19:30 +0300

    Добавлен класс планшеты

---
+++ Laptop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_2_Polimorfizm
+{
+    class Laptop : Product
+    {
+        public string Material { get; set; }
+        public Laptop(string name, double price, string manufacture, string material)
+            : base(name, price, manufacture)
+        {
+            Material = material;
+        }
+    }
+}
