c0faf10e9946edcde74f91b32542939c9d085492
AlexSemynin <alex.semynin97@mail.ru>
2019-02-06 23:06:49 +0300

    Добавлен конструктор ( ТОчно ) для родительского класса Product

--- Product.cs
+++ Product.cs
@@ -8,10 +8,12 @@ namespace _4_4_2_Polimorfizm
 {
     class Product
     {
+        //свойства
         public string Name { get; set; }
         public double Price { get; set; }
         public string Manufacturer { get; set; }
 
+        //метод
         public double GetDiscountPrice(User user) //сюда передаем копию ссылки на объект
         {
             if (user.Spent < 300)
@@ -23,6 +25,13 @@ namespace _4_4_2_Polimorfizm
                 return 0.9 * Price;
             }
             return 0.8 * Price;
+        }
+        //Конструктор
+        public Product(string name, double price, string manufacture)
+        {
+            Name = name;
+            Price = price;
+            Manufacturer = manufacture;
         }
     }
 }
