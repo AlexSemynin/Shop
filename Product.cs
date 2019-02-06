2809be2b15b28ef4f4da414252864a138208b940
AlexSemynin <alex.semynin97@mail.ru>
2019-02-06 22:59:59 +0300

    Добавлен конструктор для родительского класса Product

---
+++ Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_2_Polimorfizm
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Manufacturer { get; set; }
+
+        public double GetDiscountPrice(User user) //сюда передаем копию ссылки на объект
+        {
+            if (user.Spent < 300)
+            {
+                return Price;
+            }
+            if (user.Spent < 1000)
+            {
+                return 0.9 * Price;
+            }
+            return 0.8 * Price;
+        }
+    }
+}
